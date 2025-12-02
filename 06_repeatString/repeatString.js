a3b437a7328aa9cf908470c38e01c3b480505b9c
Hao <honghao0413@gmail.com>
2025-12-02 18:29:32 -0500

    did javascript exercise 06_repeatstring

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = "";
+    for (let i = 0; i < num; i++) {
+        string += str;
+    }
+    return string
 };
+
+console.log(repeatString('hey', 3)) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
