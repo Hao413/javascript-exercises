0fba2da2a53040fe8752d2ffa138925ac23f174d
Hao <honghao0413@gmail.com>
2025-12-03 11:54:53 -0500

    did reverseString used split ( turn into array), reverse ( reverse array), and join (turn to string)

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // split turns string to array of characters, reverse can now work
+    // and then join to bring to back to a string
+   console.log(str.split("").reverse().join(""));
 };
+
+reverseString("hello world");
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
