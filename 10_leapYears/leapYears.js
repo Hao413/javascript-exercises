56a9a8fc987c149b0475c8d907c68d65867c6021
Hao <honghao0413@gmail.com>
2025-12-03 19:42:50 -0500

    the if statement: if year is divisible by 4, and divisible by 100 unless it's also divisible by 400 is a leap year. Year like 1800 or 1900 which is divisible by 100 but not 400 is not a leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
+/* Create a function that determines whether or not a given 
+year is a leap year. Leap years are determined by the 
+following rules:
+
+> Leap years are years divisible by four (like 1984 and 2004).
+However, years divisible by 100 are not leap years (such as 
+1800 and 1900) unless they are divisible by 400 
+(like 1600 and 2000, which were in fact leap years). 
+*/
+
+const leapYears = function (year) {
+    if (year % 4 == 0 && !year % 100 == 0 || year % 400 == 0) {
+        console.log(year + " is a leap year")
+    }
+    else {
+        console.log(year + " is not a leap year");
+    }
 
 };
+
+leapYears(2000) // is a leap year: returns true
+leapYears(1985) // is not a leap year: returns false
+
 
 // Do not edit below this line
 module.exports = leapYears;
