782d4c2d691f0f3083db514ef2ca90f3dace770a
Hao <honghao0413@gmail.com>
2025-12-03 19:24:16 -0500

    add a sum variable to store the total of two positive integers with all the numbers in between. set i to min and i less than max and increment i and sum catches all of i.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns
+// the sum of every integer between (and including) them:
 
+const sumAll = function (min, max) {
+
+    // if statements to make sure the arguments are legit
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    console.log(sum);
 };
+
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
