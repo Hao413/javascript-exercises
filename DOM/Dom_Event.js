79607db0c6e4b74cffce45bad987c026fd973eeb
Hao <honghao0413@gmail.com>
2025-12-08 13:28:02 -0500

    followed TOP and tried the three methods for DOM events

---
+++ DOM/Dom_Event.js
@@ -0,0 +1,10 @@
+// Method 2 the JavaScript file
+// const btn = document.querySelector("#btn");
+// btn.onclick = () => alert("Hello World");
+
+
+// Method 3 the JavaScript file
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  alert("Hello World");
+});
