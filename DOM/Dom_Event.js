76173359af9dba96a480abf8c1c01c721850cc4d
Hao <honghao0413@gmail.com>
2025-12-10 13:33:37 -0500

    followed SuperSimpleDev with rockpaperscissors project using document querySelector innerText. They used localStorage element and JSON

--- DOM/Dom_Event.js
+++ DOM/Dom_Event.js
@@ -4,7 +4,31 @@
 
 
 // Method 3 the JavaScript file
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   alert("Hello World");
+// });
+
+
+// Method 1 - Using with named function
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+// METHODS 2 & 3
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
 const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-  alert("Hello World");
+
+// METHOD 2
+btn.onclick = alertFunction;
+
+// METHOD 3
+btn.addEventListener("click", alertFunction);
+
+btn.addEventListener("click", function (e) {
+  console.log(e);
 });
+
+
